1f004f4cc4b00d4e85cfbbc811fd70acb4089c0b
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-05-05 13:35:57 +0000

    Add django form to template

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Item
-# Create your views here.
+from .forms import ItemForm
 
 
 def get_todo_list(request):
@@ -11,11 +11,15 @@ def get_todo_list(request):
     return render(request, 'todo/todo_list.html', context)
 
 
-def add_item(request):
-    if request.method == "POST":
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
-        return redirect('get_todo_list')
-    return render(request, 'todo/add_item.html')
 
+def add_item(request):
+    if request.method == 'POST':
+        form  = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
